54c9a3e709777257971fb91f15bfb39128aa2359
Tal Malchi <malchital@gmail.com>
2022-03-06 16:47:11 +0200

    final

--- Ex1/Part_A/mat.cpp
+++ Ex1/Part_A/mat.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Tal on 3/6/2022.
-//
+#include "mat.hpp"
+using namespace ariel;
+
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+std::string ariel::mat(int a, int b, char c, char d){
+    
+    if (a%2==0 || b%2==0){
+        throw invalid_argument("number must odd");
+    }
+     return "a";
+    
+    };
 
--- Ex1/Part_A/mat.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Tal on 3/6/2022.
-//
-
-#ifndef CPP_MAT_H
-#define CPP_MAT_H
-
-#endif //CPP_MAT_H
